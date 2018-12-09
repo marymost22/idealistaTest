da71c79d844a08ead537e8c03636e45ce6cb6c0c
Mary Luz Mosteiro Del Pilar <marymost22@gmail.com>
2018-12-09 02:19:47 +0100

    Correction in ScoreTest.
    After debugging and after taking into account the final implementation of the algorthms, the tests needed a refactorization and, in some cases,a reimplementation.

--- src/test/java/ScoresTest.java
+++ src/test/java/ScoresTest.java
@@ -2,21 +2,29 @@ import model.Chalet;
 import model.Flat;
 import model.Garage;
 import model.Photo;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 public class ScoresTest {
+    private Manager manager = Manager.getInstance();
 
+    @Before
+    public void setUp() {
+        manager.setPhotosArray((ArrayList<Photo>) null);
+    }
 
     @Test
     public void checkNoPhotoAddScore() {
         Flat flatTest = new Flat(1, null);
-        Manager manager = Manager.getInstance();
+        manager.calculateAddScore(flatTest);
+
         assertEquals("Un anuncio sin foto debe tener 0 puntos", 0, (int) flatTest.getScore());
     }
 
@@ -26,7 +34,8 @@ public class ScoresTest {
         Photo photoTest = new Photo(1, new URL("http://www.idealista.com/pictures/1"), "HD");
         flatTest.addPhoto(photoTest);
 
-        Manager manager = Manager.getInstance();
+        manager.setPhotosArray(photoTest);
+        manager.calculateAddScore(flatTest);
         assertEquals("Un anuncio con foto HD debe tener 20 puntos", 20, (int) flatTest.getScore());
     }
 
@@ -36,7 +45,9 @@ public class ScoresTest {
         Photo photoTest = new Photo(1, new URL("http://www.idealista.com/pictures/1"), "SD");
         flatTest.addPhoto(photoTest);
 
-        Manager manager = Manager.getInstance();
+        manager.setPhotosArray(photoTest);
+
+        manager.calculateAddScore(flatTest);
         assertEquals("Un anuncio con foto SD debe tener 10 puntos", 10, (int) flatTest.getScore());
     }
 
@@ -45,15 +56,15 @@ public class ScoresTest {
     public void checkFlatAddMinDescriptionScore() {
         Flat flatTest = new Flat(1, "Esta descripción es mínima. Por ello tiene menos de veinte palabras");
 
-        Manager manager = Manager.getInstance();
-        assertEquals("Un anuncio con foto descripcion mínima debe tener 5 puntos", 5, (int) flatTest.getScore());
+        manager.calculateAddScore(flatTest);
+        assertEquals("Un anuncio con descripcion mínima debe tener 5 puntos", 5, (int) flatTest.getScore());
     }
 
     @Test
     public void checkFlatAddShortDescriptionScore() {
         Flat flatTest = new Flat(1, "Esta descripción de anuncio de piso es corta. Por ello tiene por al menos veinte palabras. " +
-                "Entre ellas: Luminoso, Nuevo, Céntrico, Reformado, Ático");
-        Manager manager = Manager.getInstance();
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor");
+        manager.calculateAddScore(flatTest);
         assertEquals("Un anuncio con descripción corta debe tener 15 puntos", 15, (int) flatTest.getScore());
     }
 
@@ -61,20 +72,18 @@ public class ScoresTest {
     @Test
     public void checkFlatAddLongDescriptionScore() {
         Flat flatTest = new Flat(1, "Esta descripción de anuncio de piso es larga. Por ello tiene por al más de cincuenta palabras. " +
-                "Entre ellas: Luminoso, Nuevo, Céntrico, Reformado, Ático. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do " +
-                "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
-                "nisi ut aliquip ex ea commodo consequat. ");
-        Manager manager = Manager.getInstance();
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim " +
+                "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ");
+        manager.calculateAddScore(flatTest);
         assertEquals("Un anuncio con descripción larga debe tener 35 puntos", 35, (int) flatTest.getScore());
     }
 
     @Test
     public void checkChaletDescriptionScore() {
         Chalet chaletTest = new Chalet(1, "Esta descripción de anuncio de chalet es larga. Por ello tiene por al más de cincuenta palabras. " +
-                "Entre ellas: Luminoso, Nuevo, Céntrico, Reformado, Ático. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do " +
-                "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
-                "nisi ut aliquip ex ea commodo consequat. ");
-        Manager manager = Manager.getInstance();
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim " +
+                "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ");
+        manager.calculateAddScore(chaletTest);
         assertEquals("Un anuncio con descripción larga debe tener 25 puntos", 25, (int) chaletTest.getScore());
 
     }
@@ -82,7 +91,6 @@ public class ScoresTest {
     @Test
     public void checkNoSpecialWordsInAddScore() {
         Flat flatTest = new Flat(1, "Esta descripción de anuncio de piso es muy corta. Ninguna palabra es especial.");
-        Manager manager = Manager.getInstance();
         manager.calculateAddScore(flatTest);
         assertEquals("Un anuncio sin palabras especiales debe tener 5 puntos", 5, (int) flatTest.getScore());
     }
@@ -90,7 +98,6 @@ public class ScoresTest {
     @Test
     public void checkOneSpecialWordsInAddScore() {
         Flat flatTest = new Flat(1, "Esta descripción de anuncio de piso es muy corta. Solo con una palabra especial: Nuevo");
-        Manager manager = Manager.getInstance();
         manager.calculateAddScore(flatTest);
         assertEquals("Un anuncio con una palabra especial debe tener 10 puntos", 10, (int) flatTest.getScore());
     }
@@ -99,7 +106,6 @@ public class ScoresTest {
     public void checkAllSpecialWordsInAddScore() {
         Flat flatTest = new Flat(1, "Esta descripción de anuncio de piso es corta. Por ello tiene por al menos veinte palabras. " +
                 "Entre ellas: Luminoso, Nuevo, Céntrico, Reformado, Ático.");
-        Manager manager = Manager.getInstance();
         manager.calculateAddScore(flatTest);
         assertEquals("Un anuncio con todas las palabras especiales debe tener 40 puntos", 40, (int) flatTest.getScore());
 
@@ -121,6 +127,7 @@ public class ScoresTest {
         Photo photoTest = new Photo(1, new URL("http://www.idealista.com/pictures/1"), "SD");
         chaletTest.addPhoto(photoTest);
         chaletTest.setHouseSize(300);
+        chaletTest.setGardenSize(50);
         chaletTest.calculateCompleteAddScore();
         assertTrue("Si un anuncio de chalet tiene foto, descripción y tamaño de casa y jardín, está completo", chaletTest.isComplete());
     }
